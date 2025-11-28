7792b576c03a607a9beba2b794dc49e8b6830507
lddm4200 <151487575+lddm4200@users.noreply.github.com>
2025-11-28 11:01:00 +0700

    import tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,32 +4,36 @@ import { ThemeProvider as CustomThemeProvider } from "@/contexts/ThemeContext";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBarWrapper } from "@/components/StatusBarWrapper";
 import { View } from "react-native";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // export const unstable_settings = {
 //   anchor: "(tabs)",
 // };
 
+const queryClient = new QueryClient()
 export default function RootLayout() {
   const edgeInsets = useSafeAreaInsets();
-  const token = null; // Replace with actual authentication logic
+  const token = "null"; // Replace with actual authentication logic
 
   return (
-    <CustomThemeProvider>
-      <StatusBarWrapper />
-      <View style={{ marginTop: edgeInsets.top, flex:1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Protected guard={!token}>
-            <Stack.Screen name="(auth)" />
-          </Stack.Protected>
-          <Stack.Protected guard={!!token}>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen
-              name="modal"
-              options={{ presentation: "modal", title: "Modal" }}
-            />
-          </Stack.Protected>
-        </Stack>
-      </View>
-    </CustomThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <CustomThemeProvider>
+        <StatusBarWrapper />
+        <View style={{ marginTop: edgeInsets.top, flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Protected guard={!token}>
+              <Stack.Screen name="(auth)" />
+            </Stack.Protected>
+            <Stack.Protected guard={!!token}>
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen
+                name="modal"
+                options={{ presentation: "modal", title: "Modal" }}
+              />
+            </Stack.Protected>
+          </Stack>
+        </View>
+      </CustomThemeProvider>
+    </QueryClientProvider>
   );
 }
