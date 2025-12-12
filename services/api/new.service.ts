2d64b668cc55feb5ba7c55be648a0058f4f0e783
lddm4200 <151487575+lddm4200@users.noreply.github.com>
2025-12-12 17:09:08 +0700

    call api news

---
+++ services/api/new.service.ts
@@ -0,0 +1,38 @@
+import AppConfig from '@/config/app.config';
+import { NewsArticle, PaginatedResponse } from '@/types';
+import qs from 'qs'
+
+const BASE_API_URL = AppConfig.api.baseUrl
+class NewService {
+    /**
+     * Get all NewsArticle
+     */
+    async getNews(params?: {
+        page?: number;
+        status?: string;
+        search?: string;
+    }): Promise<PaginatedResponse<NewsArticle>> {
+        const queryString = qs.stringify(params)
+        const url = `${BASE_API_URL}news?${queryString}`;
+
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Failed to fetch: ${response.status}`);
+
+        return response.json();
+    }
+
+    /**
+     * Get NewsArticle by ID
+     */
+    async getNewById(id: string): Promise<NewsArticle> {
+        const response = await fetch(`${BASE_API_URL}news/${id}`);
+        if (!response.ok) {
+            throw new Error('Không thể tải chi tiết news');
+        }
+        const result: { data: NewsArticle } = await response.json();
+        return result.data;
+    }
+
+}
+
+export default new NewService();
